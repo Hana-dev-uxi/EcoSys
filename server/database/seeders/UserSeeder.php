1e46eb7a77c1a64b547a38f77381066e87d1670d
HanaCreates <simobelhaj199777@gmail.com>
2026-06-10 15:29:51 +0100

    Create UserSeeder.php

---
+++ server/database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name'       => 'Admin User',
+                'role'       => 'admin',
+                'email'      => 'admin@ecosys.com',
+                'password'   => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name'       => 'Test User',
+                'role'       => 'user',
+                'email'      => 'user@ecosys.com',
+                'password'   => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
