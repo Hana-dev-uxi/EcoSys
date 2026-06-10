2f639de381476958e1888b03b8e038cc8a465451
HanaCreates <simobelhaj199777@gmail.com>
2026-06-10 15:09:14 +0100

    Update products-actions.js

--- client/src/store/actions/products-actions.js
+++ client/src/store/actions/products-actions.js
@@ -9,7 +9,6 @@ export const getProducts = () => {
     return async dispatch => {
         dispatch(uiActions.productsLoading());
         const fetchData = async () => {
-            const response = await axios.get('http://localhost:8000/api/products');
 
             const data = await response.data;
             return data;
